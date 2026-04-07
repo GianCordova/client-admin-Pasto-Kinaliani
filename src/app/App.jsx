ea1e936ee94d7288d24d1a36de34fb91715f5b40
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-04-07 10:18:45 -0600

    Se subio la base del proyecto

---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import { AuthPage } from "../features/auth/pages/AuthPage.jsx";
+import { LoginForm } from "../features/auth/components/LoginForm.jsx";
+function App() {
+    return (
+        <>
+          <h1>Hola Mundo</h1>
+          <AuthPage />
+          <LoginForm />
+        </>
+  );
+}
+
+export default App;
