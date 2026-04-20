1e52ac368c814b9c02a452a7c7a8734a57b2e98a
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-04-20 09:18:56 -0600

    Se modifico elDasboardContainer

--- src/shared/components/layout/DashboardContainer.jsx
+++ src/shared/components/layout/DashboardContainer.jsx
@@ -2,29 +2,38 @@ import { useState } from "react";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
 
-// IMPORTA TU VISTA
 import { ReservacionesPage } from "../../../features/reservaciones/pages/ReservacionesPage.jsx";
+// importa más vistas aquí cuando las tengas
+// import { EquiposPage } from "...";
 
 export const DashboardContainer = () => {
 
   const [view, setView] = useState("Reservaciones");
 
-  return(
+  const renderView = () => {
+    switch (view) {
+
+      case "Reservaciones":
+        return <ReservacionesPage />;
+
+      default:
+        return <h2 className="text-gray-500">Vista no encontrada</h2>;
+    }
+  };
+
+  return (
     <div className="min-h-screen bg-gradient-to-br from-orange-500 via-orange-400 to-blue-500 flex flex-col">
-      
-      {/* Navbar */}
+
       <Navbar />
 
       <div className="flex flex-1">
-        
-        {/* Sidebar */}
+
         <Sidebar setView={setView} />
 
         <main className="flex-1 p-6 bg-white/90 rounded-tl-2xl">
-          
-          {view === "Reservaciones" && <ReservacionesPage />}
-
+          {renderView()}
         </main>
+
       </div>
     </div>
   );
