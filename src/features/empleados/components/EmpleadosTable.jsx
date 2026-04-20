df96432f01c37febbac2e5ecd78595c057cc9452
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-04-20 09:12:02 -0600

    Se completo el dashboard de Empleados

--- src/features/empleados/components/EmpleadosTable.jsx
+++ src/features/empleados/components/EmpleadosTable.jsx
@@ -1,41 +1,41 @@
 import { TableTemplate } from "../../../shared/components/ui/TableTemplate";
 
 export const EmpleadosTable = () => {
-  const columns = ["Nombre", "Apellido", "DPI", "Puesto", "Sueldo", "Estado"];
+  const columns = ["name", "surname", "dpi", "puesto", "sueldo", "estado"];
 
   const data = [
     {
-      nombre: "Juan",
-      apellido: "Pérez",
+      name: "Juan",
+      surname: "Pérez",
       dpi: "1234 56789 0101",
       puesto: "CHEF",
       sueldo: "Q 5,500",
       estado: "Activo"
     },
     {
-      nombre: "María",
-      apellido: "López",
+      name: "María",
+      surname: "López",
       dpi: "9876 54321 0202",
       puesto: "MESERO",
       sueldo: "Q 3,200",
-      estado: "Activo"
+      estado: "Inactivo"
     }
   ];
 
   const formattedData = data.map(emp => ({
-    col1: emp.nombre,
-    col2: emp.apellido,
+    col1: emp.name,
+    col2: emp.surname,
     col3: emp.dpi,
     col4: emp.puesto,
     col5: emp.sueldo,
-    col6: emp.estado
+    col6: emp.status
   }));
 
   return (
     <TableTemplate
-      title="Gestión de Personal"
+      title="Gestion de Personal"
       columns={columns}
-      data={formattedData}
+      data={data}
     />
   );
 };
--- src/shared/components/layout/DashboardContainer.jsx
+++ src/shared/components/layout/DashboardContainer.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Navbar } from "./Navbar";
 import { Sidebar } from "./Sidebar";
 
+import { EmpleadosPage } from "../../../features/empleados/pages/EmpleadosPage.jsx";
+
 export const DashboardContainer = () => {
+  const [view, setView] = useState("Empleados");
 
   return(
     <div className="min-h-screen bg-gradient-to-br from-orange-500 via-orange-400 to-blue-500 flex flex-col">
@@ -10,11 +14,10 @@ export const DashboardContainer = () => {
 
       <div className="flex flex-1">
         {/* Sidebar */}
-        <Sidebar />
+        <Sidebar setView={setView} />
 
         <main className="flex-1 p-6 bg-white/90 rounded-tl-2xl">
-          {/* Children */}
-          Contenido del menú PastoKinalini
+          {view === "Empleados" && <EmpleadosPage />}
         </main>
       </div>
     </div>
