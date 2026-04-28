ab20c7c89367bb3df6d3caac62344f502afc44e2
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-04-27 18:37:22 -0600

    Se Modifico AppRoutes para añadir rutas de cada modulo

--- src/app/router/AppRoutes.jsx
+++ src/app/router/AppRoutes.jsx
@@ -1,17 +1,38 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { AuthPage } from "../../features/auth/pages/AuthPage.jsx";
-import { DashboardPage } from "../layouts/DashboardPage.jsx";
+import { DashboardContainer } from "../../shared/components/layout/DashboardContainer.jsx";
+import { Empleados } from "../../features/empleados/components/Empleados.jsx";
+import { Inventario } from "../../features/inventario/components/Inventario.jsx";
+import { Mesas } from "../../features/mesas/components/Mesas.jsx";
+import { Pedidos } from "../../features/pedidos/components/Pedidos.jsx";
+import { Platillos } from "../../features/platillos/components/Platillos.jsx";
+import { Proveedores } from "../../features/proveedores/components/Proveedores.jsx";
+import { Reservaciones } from "../../features/reservaciones/components/Reservaciones.jsx";
+import { Sucursales } from "../../features/sucursales/components/Sucursales.jsx";
+import { Usuarios } from "../../features/usuarios/components/Usuarios.jsx";
+import { Ventas } from "../../features/ventas/components/Ventas.jsx";
 
 export const AppRoutes = () => {
 
-    return(
+    return (
         <Routes>
             {/* Publicas */}
-            <Route path="/" element={<AuthPage />}/>
+            <Route path="/" element={<AuthPage />} />
 
             {/* Protegido por role */}
-            <Route path="/dashboard/*" element={<DashboardPage />}/>
-            
+            <Route path="/dashboard" element={<DashboardContainer />}>
+                <Route path="empleados" element={<Empleados />} />
+                <Route path="inventario" element={<Inventario />} />
+                <Route path="mesas" element={<Mesas />} />
+                <Route path="pedidos" element={<Pedidos />} />
+                <Route path="platillos" element={<Platillos />} />
+                <Route path="proveedores" element={<Proveedores />} />
+                <Route path="reservaciones" element={<Reservaciones />} />
+                <Route path="sucursales" element={<Sucursales />} />
+                <Route path="usuarios" element={<Usuarios />} />
+                <Route path="ventas" element={<Ventas />} />
+            </Route>
+
         </Routes>
     );
 }
