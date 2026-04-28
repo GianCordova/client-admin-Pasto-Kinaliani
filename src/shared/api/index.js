d5ae89b64a52d81ddca331761f213ce8302b3058
FernanMaldonado <fmaldonado-2024021@kinal.edu.gt>
2026-04-27 18:38:01 -0600

    Se pulieron detalles de Login

---
+++ src/shared/api/index.js
@@ -0,0 +1,5 @@
+export * from "./auth.js";
+// export * from "./admin";
+// export { axiosAuth, axiosAdmin, handleRefreshToken } from "./api";
+export { axiosAuth, handleRefreshToken } from "./api.js";
+export * from "./api.js" 
